a6ba222b353b18a38d95eb876ab61a1d0f8573e3
dw-wsb <133579580+Alum782@users.noreply.github.com>
2023-06-04 16:45:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+text
+"""
+
 from flask import Flask, render_template
 from markupsafe import escape
 app = Flask(__name__)
@@ -5,10 +9,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """tekst2"""
     return render_template('index.html')
 
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello(name=Damian):
+    """tekst3"""
     return render_template('hello.html', name=name)
