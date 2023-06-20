91b8c0d5093c78252179fbcb7dadd4228cc28a85
dw-wsb <133579580+Alum782@users.noreply.github.com>
2023-06-20 12:24:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-from flask import Flask, render_template
-from markupsafe import escape
-
+from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name='Damian'):
-    return render_template('hello.html', name=escape(name))
-
-if __name__ == '__main__':
-    app.run(port=5000)
+return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
+if __name__ == "__main__":
+app.run(debug=True)
